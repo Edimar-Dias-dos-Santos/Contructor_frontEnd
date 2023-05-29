6bf34e5009cebf916f2e01b748ca7f491ad40529
Edimar Dias dos Santos <97312864+Edimar-Dias-dos-Santos@users.noreply.github.com>
2023-05-29 00:06:46 -0300

    modificações importantes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
-import { HomeComponent } from './home/home.component';
-import { SugestoesComponent } from './sugestoes/sugestoes.component';
-import { CategoriasComponent } from './categorias/categorias.component';
-import { DestaquesComponent } from './destaques/destaques.component';
-import { CadastroComponent } from './cadastro/cadastro.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './Components/home/home.component';
+import { SugestoesComponent } from './Components/sugestoes/sugestoes.component';
+import { CategoriasComponent } from './Components/categorias/categorias.component';
+import { CadastroComponent } from './Components/cadastro/cadastro.component';
+import { LoginComponent } from './Components/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -22,10 +21,6 @@ const routes: Routes = [
   {
     path: "cadastrar",
     component: CadastroComponent
-  },
-  {
-    path: "destaques",
-    component: DestaquesComponent
   },
   {
     path: "categorias",
