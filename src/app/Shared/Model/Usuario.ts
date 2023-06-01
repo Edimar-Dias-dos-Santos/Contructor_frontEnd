2bde7cae514beecc6f5e43765da08b20fb6e5746
Edimar <eduardo.santos@useall.com.br>
2023-06-01 13:44:15 -0300

    integração1

--- src/app/Shared/Model/Usuario.ts
+++ src/app/Shared/Model/Usuario.ts
@@ -1,4 +1,4 @@
-export interface Ususario{
+export interface Usuario{
 
     id: number;
     nome: string;
--- src/app/Shared/Service/serv-usuario.service.ts
+++ src/app/Shared/Service/serv-usuario.service.ts
@@ -1,22 +1,26 @@
-import { HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../Model/Usuario';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ServUsuarioService {
+export class ServUsuario {
 
-  
-  apiUrl = 'http//localhost:8080/constructor/api/usuario';
+  apiUrl = 'http://localhost:8080/constructor/api/usuario';
 
   httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type' : 'application/json'
-
+      'Content-Type': 'application/json'
     })
   };
 
   constructor(
-    private httpClient : HttpClientModule
+    private httpClient: HttpClient
   ) { }
+
+  public getUsuarios(flag: string): Observable<Usuario[]> {
+    return this.httpClient.get<Usuario[]>(`${this.apiUrl}?flag=${flag}`);
+  }
 }
