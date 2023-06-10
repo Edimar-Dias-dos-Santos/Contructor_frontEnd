c1ba4284e2b23cec160a20b13fc4026ed2a07570
Edimar Dias dos Santos <97312864+Edimar-Dias-dos-Santos@users.noreply.github.com>
2023-06-10 18:20:54 -0300

    iniciado filtro

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -51,3 +51,31 @@ export class HomeComponent implements OnInit, AfterViewInit {
     });
   }
 }
+
+
+document.addEventListener('DOMContentLoaded', (): void => {
+  const carousel = document.querySelector('.carousel') as HTMLElement;
+  const carouselInner = carousel.querySelector('.carousel-inner') as HTMLElement;
+  const prevBtn = carousel.querySelector('.carousel-control.prev') as HTMLElement;
+  const nextBtn = carousel.querySelector('.carousel-control.next') as HTMLElement;
+  const itemWidth = carouselInner.offsetWidth / 3;
+  let position = 0;
+
+  prevBtn.addEventListener('click', (e): void => {
+    e.preventDefault();
+    position += itemWidth;
+    if (position > 0) {
+      position = -itemWidth * 2;
+    }
+    carouselInner.style.transform = `translateX(${position}px)`;
+  });
+
+  nextBtn.addEventListener('click', (e): void => {
+    e.preventDefault();
+    position -= itemWidth;
+    if (position < -itemWidth * 2) {
+      position = 0;
+    }
+    carouselInner.style.transform = `translateX(${position}px)`;
+  });
+});
