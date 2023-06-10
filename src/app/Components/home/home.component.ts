90bf6ba4cc556995c8002d85a0ec9d74e46b7a5c
Edimar Dias dos Santos <97312864+Edimar-Dias-dos-Santos@users.noreply.github.com>
2023-06-10 17:51:56 -0300

    installado o Isotope

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,27 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Usuario } from 'src/app/Shared/Model/Usuario';
 import { ServUsuario } from 'src/app/Shared/Service/serv-usuario.service';
+import Isotope from 'isotope-layout';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-
+export class HomeComponent implements OnInit, AfterViewInit {
   Usuarios: Usuario[] = [];
 
-  constructor(
-    public servUsuario: ServUsuario
-  ) { }
+  constructor(public servUsuario: ServUsuario) { }
 
   ngOnInit(): void {
     this.getUsuarios();
   }
 
+  ngAfterViewInit(): void {
+    const grid = new Isotope('#card-container', {
+      itemSelector: '.item',
+      layoutMode: 'fitRows',
+    });
+
+    const filterButtons = document.querySelectorAll('.filter-button');
+    filterButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        const filterValue = button.getAttribute('data-filter');
+        if (filterValue) {
+          grid.arrange({ filter: filterValue });
+        }
+      });
+    });
+
+    const sortButtons = document.querySelectorAll('.sort-button');
+    sortButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        const sortBy = button.getAttribute('data-sort-by');
+        const sortAsc = button.getAttribute('data-sort-asc') === 'true';
+        if (sortBy) {
+          grid.arrange({ sortBy: sortBy, sortAscending: sortAsc });
+        }
+      });
+    });
+  }
+
   getUsuarios() {
-    this.servUsuario.getUsuarios().subscribe(data => {
+    this.servUsuario.getUsuarios().subscribe((data) => {
       this.Usuarios = data;
     });
   }
