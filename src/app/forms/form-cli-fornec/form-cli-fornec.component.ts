300b327e739eca344ad507668aa73972dd8f4613
Edimar <eddy.development1997@gmail.com>
2023-06-24 21:54:39 -0300

    método de criar Clifornec

--- src/app/forms/form-cli-fornec/form-cli-fornec.component.ts
+++ src/app/forms/form-cli-fornec/form-cli-fornec.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ServClifonecService } from 'src/app/Shared/Service/serv-clifonec.service';
 import { CliFornec } from 'src/app/Shared/Model/CliFornec';
 import { ServLoginServiceService } from 'src/app/Shared/Service/serv-login-service.service';
+import { ServUsuario } from 'src/app/Shared/Service/serv-usuario.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { ExceptionDefault } from 'src/app/Shared/Messages/Exceptions';
 
 @Component({
   selector: 'app-form-cli-fornec',
@@ -14,7 +18,9 @@ export class FormCliFornecComponent implements OnInit {
   
 
   constructor(private cliFornecService: ServClifonecService,
-              private servLoginServiceService: ServLoginServiceService) {}
+              private servLoginServiceService: ServLoginServiceService,
+              private servUsuario: ServUsuario,
+              private router: Router) {}
 
   ngOnInit() {
 
@@ -24,7 +30,7 @@ export class FormCliFornecComponent implements OnInit {
   iniciarCliFornec(): CliFornec{
      return {
       id: '',
-      idUsuario: '',
+      idUsuario: this.servUsuario.usuarioAtual?.idUsuario || '',
       diasQueTrabalha: 0,
       descricao: '',
       valor: 0
@@ -32,6 +38,14 @@ export class FormCliFornecComponent implements OnInit {
   }
 
   setCliFornec(): void {
+
+    if (!this.servUsuario.usuarioAtual) {
+      Swal.fire('Atenção!', 'código de usuário não encontrado', 'error');
+      throw new ExceptionDefault('Por favor informe todos os campos');
+    }
+
+    this.cliFornec.idUsuario = this.servUsuario.usuarioAtual.idUsuario;
+
     this.cliFornecService.setCliFornec(this.cliFornec)
       .subscribe(
         (cliFornec: CliFornec) => {
@@ -39,6 +53,7 @@ export class FormCliFornecComponent implements OnInit {
           // Faça qualquer manipulação adicional necessária após a criação do cliFornec
         },
         (error: any) => {
+          Swal.fire('Obs!', 'Ocorreu um erro ao criar um cadastro de serviço:', 'error');
           console.error('Erro ao criar CliFornec:', error);
           // Lide com o erro de forma apropriada
         }
