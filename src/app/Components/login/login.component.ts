88b9ec40a092aa9eb69ad4d24357eaf63c6edee3
Edimar <eduardo.santos@useall.com.br>
2023-06-26 13:03:06 -0300

    login pt1

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ServLoginServiceService } from 'src/app/Shared/Service/serv-login-service.service';
+import { Login } from 'src/app/Shared/Model/Login';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  constructor(private loginService: ServLoginServiceService) { }
+
+  autenticarUsuario(username: string, password: string): void {
+    this.loginService.authenticateUser(username, password).subscribe(
+      (login: Login) => {
+        // Autenticação bem-sucedida, redirecione para outra rota
+      },
+      (error) => {
+        alert('Usuário não existe!');
+      }
+    );
+  }
 
 }
--- src/app/Shared/Service/serv-login-service.service.ts
+++ src/app/Shared/Service/serv-login-service.service.ts
@@ -11,8 +11,29 @@ export class ServLoginServiceService {
   private apiUrl = 'http://localhost:8080/api/Login';
   constructor(private http: HttpClient) { }
 
+  authenticateUser(username: string, password: string): Observable<Login> {
+    const loginRecordDto = { username, password };
+    return this.http.post<Login>(`${this.apiUrl}/authenticate`, loginRecordDto);
+  }
+
   SetLogin(login: Login): Observable<Login> {
     return this.http.post<Login>(this.apiUrl, login);
+  }
+
+  getLogins(): Observable<Login[]> {
+    return this.http.get<Login[]>(this.apiUrl);
+  }
+
+  getLoginById(id: string): Observable<Login> {
+    return this.http.get<Login>(`${this.apiUrl}/${id}`);
+  }
+
+  updateLogin(id: string, updatedLogin: Login): Observable<Login> {
+    return this.http.put<Login>(`${this.apiUrl}/${id}`, updatedLogin);
+  }
+
+  deleteLogin(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 
 }
