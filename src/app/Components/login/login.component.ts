34c8aa89239e56dd766d7117081f693641a26d5e
EDIMAR DIAS DOS SANTOS <edimar.santos@alunos.sc.senac.br>
2023-06-26 20:45:14 -0300

    login

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ServLoginServiceService } from 'src/app/Shared/Service/serv-login-service.service';
 import { Login } from 'src/app/Shared/Model/Login';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -15,6 +16,7 @@ export class LoginComponent {
     this.loginService.authenticateUser(username, password).subscribe(
       (login: Login) => {
         // Autenticação bem-sucedida, redirecione para outra rota
+        Swal.fire('Atenção!', 'chamar proximo component', 'success');
       },
       (error) => {
         alert('Usuário não existe!');
