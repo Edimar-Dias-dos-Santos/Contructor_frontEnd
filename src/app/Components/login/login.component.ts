653decfca87d15fe4b5a79db40399315d07817ab
Edimar <eduardo.santos@useall.com.br>
2023-06-27 13:15:42 -0300

    Anotações de melhora no login

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ServLoginServiceService } from 'src/app/Shared/Service/serv-login-service.service';
 import { Login } from 'src/app/Shared/Model/Login';
 import Swal from 'sweetalert2';
+import { ServLoginConstructoAdmService } from 'src/app/Shared/Service/serv-login-constructo-adm.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,19 +12,33 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-  constructor(private loginService: ServLoginServiceService, private router: Router) { }
+  constructor(private loginService: ServLoginServiceService, private loginAdmService: ServLoginConstructoAdmService, private router: Router) { }
 
   autenticarUsuario(username: string, password: string): void {
-    this.loginService.authenticateUser(username, password).subscribe(
-      (login: Login) => {
-        // Autenticação bem-sucedida, redirecione para outra rota
-        Swal.fire('Atenção!', 'chamar proximo component', 'success');
-        this.router.navigateByUrl('/ConstructorADM');
+    this.loginAdmService.authenticateUser(username, password).subscribe(
+      (loginResponse: any) => {
+        // Autenticação bem-sucedida como autenticarUsuarioAdm
+        Swal.fire('Atenção!', 'Sou autenticarUsuarioAdm', 'success');
+        // Salvar o código de resposta em uma variável, se necessário
+        const responseCode = loginResponse.code;
+        // Realizar as ações específicas para autenticarUsuarioAdm
+        // ...
       },
       (error) => {
-        alert('Usuário não existe!');
+        // Não autenticado como autenticarUsuarioAdm, tentar autenticar como autenticarUsuario
+        this.loginService.authenticateUser(username, password).subscribe(
+          (login: Login) => {
+            // Autenticação bem-sucedida como autenticarUsuario
+            Swal.fire('Atenção!', 'Sou autenticarUsuario', 'success');
+            // Realizar as ações específicas para autenticarUsuario
+            // ...
+          },
+          (error) => {
+            // Não autenticado como autenticarUsuario
+            alert('Eu não existo!');
+          }
+        );
       }
     );
   }
-
 }
