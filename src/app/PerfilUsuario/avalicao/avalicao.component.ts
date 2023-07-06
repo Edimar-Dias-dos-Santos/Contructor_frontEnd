9096132718413caf815a5ddd4b2ca6b7c7edeec3
Edimar <eduardo.santos@useall.com.br>
2023-07-06 18:23:44 -0300

    ajustes final feedback

--- src/app/PerfilUsuario/avalicao/avalicao.component.ts
+++ src/app/PerfilUsuario/avalicao/avalicao.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiceAvaliacaoService } from 'src/app/Shared/Service/service-avalicao.service';
+import { Avaliacao } from 'src/app/Shared/Model/Avaliacao';
 
 @Component({
   selector: 'app-avalicao',
   templateUrl: './avalicao.component.html',
   styleUrls: ['./avalicao.component.scss']
 })
-export class AvalicaoComponent {
+export class AvalicaoComponent implements OnInit {
+  avaliacoes: Avaliacao[] = [];
 
+  constructor(private avaliacaoService: ServiceAvaliacaoService) {}
+
+  ngOnInit() {
+    this.carregarAvaliacoes();
+  }
+
+  carregarAvaliacoes() {
+    this.avaliacaoService.getAllAvaliacoes().subscribe(
+      (data: Avaliacao[]) => {
+        this.avaliacoes = data;
+      },
+      (error) => {
+        console.error('Erro ao carregar avaliações:', error);
+      }
+    );
+  }
+
+  deletarAvaliacao(id: string) {
+    this.avaliacaoService.deleteAvaliacao(id).subscribe(
+      () => {
+        // Atualiza a lista de avaliações após a exclusão
+        this.carregarAvaliacoes();
+      },
+      (error) => {
+        console.error('Erro ao deletar avaliação:', error);
+      }
+    );
+  }
 }
